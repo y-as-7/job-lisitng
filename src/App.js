6e037769841b234b1e467c84d5bba4e66d6864e5
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-03-11 04:00:18 +0200

    did the filter and finish the whole app

--- src/App.js
+++ src/App.js
@@ -6,18 +6,75 @@ function App() {
   const [jobs, setJobs] = useState([]);
   useEffect(() => setJobs(data), []);
 
-  return (
-    <div className="bg-blue-100">
-      <header className="bg-teal-500 mb-12 ">
-        <img src="/images/bg-header-desktop.svg" alt=""/>
-      </header>
-      {jobs.length === 0 ? (
-        <p>jobs are fetching</p>
-      ) : (
-        jobs.map((job) => <JobBoardComponent job={job} key={job.id} />)
-      )}
-    </div>
+  const [filters, setFilters] = useState([]);
 
+  const filterFun = ({ role, level, tools, languages }) => {
+    if (filters.length === 0) {
+      return true;
+    }
+
+    const tags = [role, level];
+
+    if (tools) {
+      tags.push(...tools);
+    }
+
+    if (languages) {
+      tags.push(...languages);
+    }
+    return filters.every(filter=> tags.includes(filter));
+  };
+  const handleTagClick = (tag) => {
+    if (filters.includes(tag)) return;
+    setFilters([...filters, tag]);
+  };
+  const handleFilterClick = (passedFilter) => {
+    setFilters(filters.filter((f) => f !== passedFilter));
+  };
+  const filteredJobs = jobs.filter(filterFun);
+  const clearFilters = () => {
+    setFilters([]);
+  };
+  return (
+    <>
+      <header className="w-full bg-teal-500 mb-12 ">
+        <img src="/images/bg-header-desktop.svg" alt="" />
+      </header>
+      <div className="container m-auto">
+        {filters.length > 0 && (
+          <div className=" flex flex-wrap bg-white shadow-md flex -my-20 z-10 relative mb-16 mx-10 p-6 rounded">
+            {filters.map((filter) => (
+              <span
+                onClick={() => {
+                  handleFilterClick(filter);
+                }}
+                className="cursor-pointer text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded lg:mb-0"
+              >
+                {filter}
+                <span className="ml-2 text-1xl font-bold sm:mb-0">&times;</span>
+              </span>
+            ))}
+            <button
+              onClick={clearFilters}
+              className="font-bold text-gray-700 ml-auto"
+            >
+              Clear
+            </button>
+          </div>
+        )}
+        {jobs.length === 0 ? (
+          <p>jobs are fetching</p>
+        ) : (
+          filteredJobs.map((job) => (
+            <JobBoardComponent
+              job={job}
+              key={job.id}
+              handleTagClick={handleTagClick}
+            />
+          ))
+        )}
+      </div>
+    </>
   );
 }
 
--- src/components/jobBoardComponent.js
+++ src/components/jobBoardComponent.js
@@ -16,6 +16,7 @@ function JobBoardComponent({
     languages,
     tools,
   },
+  handleTagClick,
 }) {
   const tags = [role,level];
   if (languages) {
@@ -29,11 +30,11 @@ function JobBoardComponent({
     <div
       className={`flex flex-col bg-white shadow-md mx-10 my-16 p-6 rounded ${
         featured && "border-l-4 border-teal-500 border-solid"
-      } sm:flex-row`}
+      } lg:flex-row`}
     >
       <div>
         <img
-          className="-mt-16 mb-4 w-20 h-20  sm:h-24 sm:w-24 sm:my-0"
+          className="-mt-16 mb-4 w-20 h-20  lg:h-24 lg:w-24 lg:my-0"
           src={logo}
           alt={company}
         />
@@ -42,12 +43,12 @@ function JobBoardComponent({
         <h3 className="font-bold text-teal-500">
           {company}
           {isNew && (
-            <span className="bg-teal-500 text-teal-100 font-bold m-2 px-2 py-1 rounded-full uppercase text-sm ">
+            <span className="bg-teal-500 text-teal-100 font-bold m-2 px-2 py-1 rounded-full uppercase text-lg ">
               new
             </span>
           )}
           {featured && (
-            <span className="bg-gray-800 text-white font-bold m-2 px-2 py-1 rounded-full uppercase text-sm">
+            <span className="bg-gray-800 text-white font-bold m-2 px-2 py-1 rounded-full uppercase text-lg">
               featured
             </span>
           )}
@@ -57,10 +58,10 @@ function JobBoardComponent({
           {postedAt} &middot; {contract} &middot; {location}
         </p>
       </div>
-      <div className="flex flex-wrap  items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-soild sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0 ">
+      <div className="cursor-pointer flex flex-wrap  items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-soild lg:ml-auto lg:border-0 lg:pt-0 lg:mt-0 ">
         {tags
           ? tags.map((tag) => (
-              <span className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0">
+              <span onClick={()=> handleTagClick(tag)} className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded lg:mb-0">
                 {tag}
               </span>
             ))
