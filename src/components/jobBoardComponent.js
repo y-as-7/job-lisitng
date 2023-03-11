dcfcd1a65b1458eb4b51b7c33506e2f2f06726eb
Youssef Askar <82427372+y-as-7@users.noreply.github.com>
2023-03-11 02:53:56 +0200

    make the app responsive

--- src/components/jobBoardComponent.js
+++ src/components/jobBoardComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function JobBoardComponent({
   job: {
+    id,
     company,
     logo,
     isNew,
@@ -25,35 +26,41 @@ function JobBoardComponent({
   }
   
   return (
-    <div className={`flex bg-white shadow-md m-4 p-6 rounded ${featured&& 'border-l-4 border-teal-500 border-solid' }`}>
+    <div
+      className={`flex flex-col bg-white shadow-md mx-10 my-16 p-6 rounded ${
+        featured && "border-l-4 border-teal-500 border-solid"
+      } sm:flex-row`}
+    >
       <div>
-        <img src={logo} alt={company} />
+        <img
+          className="-mt-16 mb-4 w-20 h-20  sm:h-24 sm:w-24 sm:my-0"
+          src={logo}
+          alt={company}
+        />
       </div>
       <div className="ml-4 flex flex-col  justify-between ">
         <h3 className="font-bold text-teal-500">
           {company}
           {isNew && (
-            <span className="bg-teal-500 text-teal-100 font-bold m-2 px-2 py-1 rounded-full ">
-              {" "}
+            <span className="bg-teal-500 text-teal-100 font-bold m-2 px-2 py-1 rounded-full uppercase text-sm ">
               new
             </span>
           )}
           {featured && (
-            <span className="bg-gray-800 text-white font-bold m-2 px-2 py-1 rounded-full">
-              {" "}
+            <span className="bg-gray-800 text-white font-bold m-2 px-2 py-1 rounded-full uppercase text-sm">
               featured
             </span>
           )}
         </h3>
-        <h2 className="font-bold text-xl ">{position}</h2>
+        <h2 className="font-bold text-xl my-2 ">{position}</h2>
         <p className="text-gray-700">
           {postedAt} &middot; {contract} &middot; {location}
         </p>
       </div>
-      <div className="flex  items-center justify-end ml-auto">
+      <div className="flex flex-wrap  items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-soild sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0 ">
         {tags
           ? tags.map((tag) => (
-              <span className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded ">
+              <span className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0">
                 {tag}
               </span>
             ))
